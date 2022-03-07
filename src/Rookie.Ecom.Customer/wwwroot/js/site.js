bd29a53e684aecec00b1ae0208a2d492f4861198
tinySharkk <dongocthanh40@gmail.com>
2022-03-07 10:01:00 +0700

    basic header

--- src/Rookie.Ecom.Customer/wwwroot/js/site.js
+++ src/Rookie.Ecom.Customer/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 10) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
