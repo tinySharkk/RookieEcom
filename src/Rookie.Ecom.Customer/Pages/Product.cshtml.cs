2d444756b4c6c999fe8f0b19944a41865c6d71c9
tinySharkk <dongocthanh40@gmail.com>
2022-03-07 13:53:42 +0700

    add product, product detail page, display feature product in home page

---
+++ src/Rookie.Ecom.Customer/Pages/Product.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Rookie.Ecom.Customer.Pages
+{
+    public class ProductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
