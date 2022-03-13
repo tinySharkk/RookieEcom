bdf442cbad78b5d4fec0f6dbef4d7ef33094b8f7
tinySharkk <dongocthanh40@gmail.com>
2022-03-13 21:43:48 +0700

    IdentitySever4 use SQL sever

---
+++ src/Rookie.Ecom.Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Rookie.Ecom.Identity.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            //code first
+            //db first
+            //model first
+        }
+
+    }
+}
--- src/Rookie.Ecom.Identity/Startup.cs
+++ src/Rookie.Ecom.Identity/Startup.cs
@@ -3,15 +3,39 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Rookie.Ecom.Identity.Data;
+using IdentityServer4.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.Linq;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 
 namespace Rookie.Ecom.Identity
 {
     public class Startup
     {
+        public IWebHostEnvironment _webHostEnvironment { get; }
+        public IConfiguration _configuration { get; }
+        public Startup(IWebHostEnvironment webHostEnvironment, IConfiguration configuration)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = _configuration.GetConnectionString("DbConnection");
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+
+            });
+
             services.AddCors(options =>
             {
                 options.AddPolicy("AllowOrigins",
@@ -28,6 +52,16 @@ namespace Rookie.Ecom.Identity
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
                 .AddTestUsers(InitData.GetUsers())
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
                 .AddInMemoryIdentityResources(InitData.GetIdentityResources())
                 .AddInMemoryClients(InitData.GetClients());
         }
@@ -54,5 +88,42 @@ namespace Rookie.Ecom.Identity
                     pattern: "{controller=home}/{action=Index}/{id?}");
             });
         }
+
+        /*private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.Ids)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.Apis)
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
+        }*/
     }
 }
